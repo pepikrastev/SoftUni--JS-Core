ee6b5bd52d39184543d91a7f7f45b82ec368cc1e
the_freestyle <the_freestyle@abv.bg>
2019-07-28 00:46:11 +0300

    Homework

---
+++ JS Apps - July/04.Routing and Architecture - Exercise/helpers/storage.js
@@ -0,0 +1,32 @@
+const storage = function () {
+
+    const appKey = "kid_HkW5XV8Gr";
+    const appSecret = "5fa1a977a61a4c5fa83e31660232bfdc";
+
+    const getData = function (key) {
+        return localStorage.getItem(key + appKey);
+    };
+
+    const saveData = function (key, value) {
+        localStorage.setItem(key + appKey, JSON.stringify(value));
+    };
+
+    const saveUser = function (data) {
+        saveData("userInfo", data);
+        saveData("authToken", data._kmd.authtoken);
+    };
+
+    const deleteUser = function () {
+        localStorage.removeItem("userInfo" + appKey);
+        localStorage.removeItem("authToken" + appKey);
+    };
+
+    return {
+        getData,
+        saveData,
+        saveUser,
+        deleteUser,
+        appKey,
+        appSecret
+    }
+}();
